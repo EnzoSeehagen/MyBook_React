73bb41e794a9be2592bff6d7a0a381bb3cef5703
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-11-10 17:28:59 -0300

    Finalizando o header

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,9 +1,10 @@
 import logo from '../../imagens/logo.svg'
+import './estilo.css'
 
 function Logo () {
     return(
         <div className='logo'>
-            <img src={logo} alt='logo'></img>
+            <img src={logo}  className='logo-img' alt='logo'></img>
             <p><strong>Alura</strong>Books</p>
         </div>
     )
